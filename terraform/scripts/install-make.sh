a93244d0215f3511c0317ef9de18c2b35e4c0452
nfstein <nicksteinhauser@gmail.com>
2021-09-21 10:41:49 -0700

    rework install script

--- terraform/scripts/install-make.sh
+++ terraform/scripts/install-make.sh
@@ -1,5 +1,11 @@
+
 # Install make
 wget http://mirror.us-midwest-1.nexcess.net/gnu/make/make-4.3.tar.gz
 tar -xvf make-4.3.tar.gz
-mv make-4.3 make
+cd make-4.3
+./configure
+sh build.sh
+./make install
 ./make --version
+mv make ..
+
