fc16824f95d496aa128ec7766bae817f55e82ff9
nfstein <nicksteinhauser@gmail.com>
2021-09-21 07:01:20 -0700

    add host setup script

--- terraform/main.tf
+++ terraform/main.tf
@@ -414,15 +414,15 @@ module "on_prem_instance" {
   bastion_host         = module.wireguard.vsi_floating_ip
 }
 
-# resource "null_resource" "make_public_location_and_cluster" {
-#   provisioner "local-exec" {
-#     command = "./scripts/install-make && make setup_terraform_env && make setup_public_hosts && make all_public_cluster"
-#     environment = {
-#       TF_VAR_RESOURCE_PREFIX  = var.TF_VAR_RESOURCE_PREFIX
-#       IC_API_KEY              = var.IC_API_KEY
-#       COS_REGION              = var.COS_REGION
-#       LOCATION_REGION         = var.LOCATION_REGION
-#       IAAS_REGION             = var.IAAS_REGION
-#     }
-#   }
-# }
+resource "null_resource" "make_public_location_and_cluster" {
+  provisioner "local-exec" {
+    command = "./scripts/install-make && make setup_public_hosts"
+    environment = {
+      TF_VAR_RESOURCE_PREFIX  = var.TF_VAR_RESOURCE_PREFIX
+      IC_API_KEY              = var.IC_API_KEY
+      COS_REGION              = var.COS_REGION
+      LOCATION_REGION         = var.LOCATION_REGION
+      IAAS_REGION             = var.IAAS_REGION
+    }
+  }
+}
